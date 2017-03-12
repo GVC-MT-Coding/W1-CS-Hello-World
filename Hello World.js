3b178c8a5ad707a6dc888e84be6573200dddf953
chanyoungs <chanyoungs@gmail.com>
2017-03-12 19:21:35 +0000

    Deleted test comments

--- Hello World.js
+++ Hello World.js
@@ -15,6 +15,3 @@ function draw() {
 
 // Here is my edit on master after I made changes on experimental branch.
 // These branches are now different.
-
-// 1235l982745987
-// ,esunthoaoeusntaoheusnt
