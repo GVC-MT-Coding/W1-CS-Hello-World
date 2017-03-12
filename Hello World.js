f7536605a779d11b09e05520af5144364760c779
chanyoungs <chanyoungs@gmail.com>
2017-03-12 18:48:55 +0000

    stnhaoe

--- Hello World.js
+++ Hello World.js
@@ -16,7 +16,4 @@ function draw() {
 // Here is my edit on master after I made changes on experimental branch.
 // These branches are now different.
 
-//Random comment
-
-//This is test branch comment
-//Anonther test commentaosneuthaosenuthaosentuhaosentuh
+// ,esunthoaoeusntaoheusnt
